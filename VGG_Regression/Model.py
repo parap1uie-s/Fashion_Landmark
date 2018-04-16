7563e77828c6a8fecd003479bcacd3fd4fb8d2e2
parap1uie-s <parap1uie-s@users.noreply.github.com>
2018-04-16 15:59:30 +0800

    update vgg based model from vgg16 to 19

--- VGG_Regression/Model.py
+++ VGG_Regression/Model.py
@@ -26,22 +26,25 @@ def change_vgg16(input_shape):
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Block 4
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
     # Block 5
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     # 使用1x1卷积
-    x = Conv2D(128, (1, 1), activation='relu', padding='same', name='block5_conv4')(x)
+    x = Conv2D(128, (1, 1), activation='relu', padding='same', name='block5_conv5')(x)
 
     # Classification block
     x = Flatten(name='flatten')(x)
--- VGG_Regression/Train.py
+++ VGG_Regression/Train.py
@@ -6,7 +6,7 @@ import os
 input_shape = (512, 512, 3)
 batch_size = 2
 model = change_vgg16(input_shape)
-weight_path = 'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+weight_path = 'vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
 model.load_weights(weight_path, by_name=True, skip_mismatch=True)
 
 if os.path.exists("vggbased_weight.h5"):
@@ -32,11 +32,3 @@ for e in range(epoch):
         # validation_data=YoloBasedGenerator(datalist, point_num, grid_length = 32, batch_size = batch_size, val_ratio = 0.9, data_type='val'), 
         # validation_steps=5)
     model.save_weights("vggbased_weight.h5")
-
-
-
-
-
-for i in range(10):
-    model.fit_generator(readImageandLandmark(file, batch_size), steps_per_epoch=100, epochs=10,use_multiprocessing=True,max_queue_size=100,workers=2)
-    model.save_weights('model.h5') 
